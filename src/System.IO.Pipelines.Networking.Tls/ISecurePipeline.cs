91716bd656bbd51b590e755c6e30a3c97718920c
Drawaes <seawardtim@gmail.com>
2016-11-20 21:48:39 +0000

    Small fixes

---
+++ src/System.IO.Pipelines.Networking.Tls/ISecurePipeline.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace System.IO.Pipelines.Networking.Tls
+{
+    public interface ISecurePipeline : IPipelineConnection
+    {
+        Task<ApplicationProtocols.ProtocolIds> ShakeHandsAsync();
+        CipherInfo CipherInfo { get; }
+    }
+}
