3e3651b5a3be07901023fc841c5d7d0fedf694b7
Drawaes <seawardtim@gmail.com>
2016-11-23 01:17:12 +0000

    Pipelines all the things

---
+++ src/System.IO.Pipelines.Networking.Tls/ISecureContext.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+
+namespace System.IO.Pipelines.Networking.Tls
+{
+    public interface ISecureContext : IDisposable
+    {
+        int TrailerSize { get; set; }
+        int HeaderSize { get; set; }
+        bool ReadyToSend { get; }
+        ApplicationProtocols.ProtocolIds NegotiatedProtocol { get; }
+        Task ProcessContextMessageAsync(ReadableBuffer readBuffer, IPipelineWriter writePipeline);
+        Task ProcessContextMessageAsync(IPipelineWriter writePipeline);
+        Task DecryptAsync(ReadableBuffer encryptedData, IPipelineWriter decryptedData);
+        Task EncryptAsync(ReadableBuffer unencrypted, IPipelineWriter encryptedData);
+        bool IsServer { get; }
+        CipherInfo CipherInfo { get; }
+    }
+}
